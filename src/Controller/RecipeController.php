f0feaac79fa99f5cbaec4f8d00220453c0500a59
Louis Roméas <louis.romeas@gmail.com>
2022-06-06 19:26:33 +0200

    redirect to recipe show after edit

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -68,7 +68,7 @@ class RecipeController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $recipeRepository->add($recipe, true);
 
-            return $this->redirectToRoute('app_recipe_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_recipe_show', [ 'id' => $recipe->getId() ], Response::HTTP_SEE_OTHER);
         }
 
         return $this->renderForm('recipe/edit.html.twig', [
