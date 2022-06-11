6f215c32d6fae0becfc834e637e5ae3d49b85352
Louis Roméas <louis.romeas@gmail.com>
2022-06-11 02:49:42 +0200

    score format done

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -4,11 +4,12 @@ namespace App\Twig;
 
 use Symfony\Bridge\Twig\Extension\TranslationExtension;
 use Twig\Extension\AbstractExtension;
+use Twig\Extra\Intl\IntlExtension;
 use Twig\TwigFilter;
 
 class AppExtension extends AbstractExtension
 {
-  public function __construct(private TranslationExtension $translationExtension)
+  public function __construct(private TranslationExtension $translationExtension, private IntlExtension $intlExtension)
   {
     
   }
@@ -17,6 +18,20 @@ class AppExtension extends AbstractExtension
     {
         return [
             new TwigFilter('t', [$this->translationExtension, 'trans']),
+            new TwigFilter('format_score', [$this, 'formatScore'])
         ];
+    }
+
+    public function formatScore(int $score) {
+        $suffixes = ['', 'k', 'M', 'B'];
+        $suffixPointer = 0;
+        while ($score >= 1000 && $suffixPointer < count($suffixes)) {
+            $score /= 1000;
+            $suffixPointer++;
+        }
+
+        return $this->intlExtension->formatNumber($score, [
+            'fraction_digit' => ( ($score >= 100 || $suffixPointer == 0) ? 0 : 1)
+        ]) . $suffixes[$suffixPointer];
     }
 }
