03ee61f2ef58c8178cab884cf4947c195bb55af6
Louis Roméas <louis.romeas@gmail.com>
2022-07-04 15:48:08 +0200

    repeat password to register

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,14 +4,18 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\EqualTo;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Contracts\Translation\TranslatorInterface;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+
+use function PHPSTORM_META\type;
 
 class RegistrationFormType extends AbstractType
 {
@@ -33,22 +37,39 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
-            ->add('plainPassword', PasswordType::class, [
+            ->add('plainPassword', RepeatedType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
+                'type' => PasswordType::class,
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
-                'constraints' => [
-                    new NotBlank([
-                        'message' => $this->translator->trans('messages.user.registration.passwordBlank'),
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
+                'options' => [
+                    'attr' => ['autocomplete' => 'new-password'],
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => $this->translator->trans('messages.user.registration.passwordBlank'),
+                        ]),
+                        new Length([
+                            'min' => 6,
+                            'minMessage' => 'Your password should be at least {{ limit }} characters',
+                            // max length allowed by Symfony for security reasons
+                            'max' => 4096,
+                        ]),
+                    ],
                 ],
+
+                'first_options' => [
+                    'label' => 'entity.user.password',
+                    'attr' => [
+                        'placeholder' => 'entity.user.password'
+                    ]
+                ],
+                'second_options' => [
+                    'label' => 'entity.user.repeatPassword',
+                    'attr' => [
+                        'placeholder' => 'entity.user.repeatPassword'
+                    ]
+                ],
+
             ])
         ;
     }
