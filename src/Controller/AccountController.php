c81aefacc137f8301b61ad90903c43fc2bba71ab
Louis Roméas <louis.romeas@gmail.com>
2022-07-21 18:12:43 +0200

    Account page

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,19 +2,71 @@
 
 namespace App\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Component\Mime\Address;
+use App\Form\PasswordChangeFormType;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 #[IsGranted('ROLE_USER')]
 class AccountController extends AbstractController
 {
-    #[Route('/account', name: 'app_account')]
-    public function index(): Response
+    #[IsGranted('ROLE_USER')]
+    #[Route('/{_locale}/account', name: 'app_account', requirements:[ '_locale' => '%app.locales%' ])]
+    public function index(
+        Request $request,
+        UserPasswordHasherInterface $hasher,
+        UserRepository $userRepository,
+        TranslatorInterface $translator,
+        MailerInterface $mailer
+    ): Response
     {
-        return $this->render('account/index.html.twig', [
-            'controller_name' => 'AccountController',
+        /** @var User $user */
+        $user = $this->getUser();
+        $passwordChangeForm = $this->createForm(PasswordChangeFormType::class, $user);
+
+        $passwordChangeForm->handleRequest($request);
+
+        if (
+            $passwordChangeForm->isSubmitted() &&
+            $passwordChangeForm->isValid()
+        ) {
+            if ($hasher->isPasswordValid($user, $passwordChangeForm->get('oldPlainPassword')->getData())) {
+                $user->setPassword(
+                    $hasher->hashPassword(
+                            $user,
+                            $passwordChangeForm->get('newPlainPassword')->getData()
+                        )
+                    );
+    
+                $userRepository->add($user, true);
+
+                // Send an e-mail to warn the user in case the password change was initiated without their consent
+                $email = (new TemplatedEmail())
+                    ->from(new Address($_ENV['admin_mail'], 'SmartChef'))
+                    ->to($user->getEmail())
+                    ->subject($translator->trans('form.passwordChangeForm.passwordChanged'))
+                    ->htmlTemplate('account/password_change_email.html.twig')
+                    ->context([
+                        'user' => $user
+                    ])
+                ;
+
+                $mailer->send($email);
+            } else $this->addFlash('form-error', 'messages.user.errors.currentPasswordIncorrect');
+        }
+
+        return $this->renderForm('account/index.html.twig', [
+            'user' => $user,
+            'passwordChangeForm' => $passwordChangeForm
         ]);
     }
 }
---
+++ src/Form/PasswordChangeFormType.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\EqualTo;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+
+use function PHPSTORM_META\type;
+
+class PasswordChangeFormType extends AbstractType
+{
+    public function __construct(private TranslatorInterface $translator)
+    {
+        
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('oldPlainPassword', PasswordType::class, [
+                'mapped' => false,
+                'label' => 'form.passwordChangeForm.currentPassword',
+                'attr' => [
+                    'autocomplete' => 'current-password',
+                    'placeholder' => 'form.passwordChangeForm.currentPassword'
+                ]
+            ])
+            ->add('newPlainPassword', RepeatedType::class, [
+                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'type' => PasswordType::class,
+                'mapped' => false,
+                'options' => [
+                    'attr' => ['autocomplete' => 'new-password'],
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => $this->translator->trans('messages.user.registration.passwordBlank'),
+                        ]),
+                        new Length([
+                            'min' => 6,
+                            'minMessage' => 'Your password should be at least {{ limit }} characters',
+                            // max length allowed by Symfony for security reasons
+                            'max' => 4096,
+                        ]),
+                    ],
+                ],
+
+                'first_options' => [
+                    'label' => 'form.resetPasswordForm.newPassword',
+                    'attr' => [
+                        'placeholder' => 'form.resetPasswordForm.newPassword'
+                    ]
+                ],
+                'second_options' => [
+                    'label' => 'form.resetPasswordForm.repeatPassword',
+                    'attr' => [
+                        'placeholder' => 'form.resetPasswordForm.repeatPassword'
+                    ]
+                ],
+
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
