9a3be4596c6cd31518c191906127ed2dd0b51c48
Louis Roméas <louis.romeas@gmail.com>
2022-07-22 15:03:31 +0200

    organized recipe thumbnail names + auto clean former

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -11,16 +11,52 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 #[Route('{_locale}/recipe', requirements:[ '_locale' => '%app.locales%' ])]
 class RecipeController extends AbstractController
 {
+    public function __construct(
+        private SluggerInterface $slugger
+    )
+    {
+        
+    }
+
+    /**
+     * @return string Full final filepath of thumbnail
+     */
+    private function handleThumbnailInput(Recipe $recipe, UploadedFile $imageFile): string {
+        // $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+        // $safeFilename = $this->slugger->slug($originalFilename);
+        $destinationDir = $this->getParameter('upload_directory') . '/recipe';
+
+        foreach (scandir($destinationDir) as $file) {
+            $filepath = $destinationDir . '/' . $file;
+            if ( is_file($filepath) && strpos( $file, $recipe->getId() . '-' ) === 0 ) unlink($filepath);
+        }
+
+        $newFilename = $recipe->getId() . '-' . time() . '-' . uniqid() . '.' . $imageFile->guessExtension();
+
+        // Move the file to the directory where brochures are stored
+        try {
+            $imageFile->move(
+                $destinationDir,
+                $newFilename
+            );
+        } catch (FileException $e) {
+            // ... handle exception if something happens during file upload
+        }
+
+        return $newFilename;
+    }
+
     #[Route('/new', name: 'app_recipe_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, RecipeRepository $recipeRepository, SluggerInterface $slugger): Response
+    public function new(Request $request, RecipeRepository $recipeRepository): Response
     {
 
         if (!$this->isGranted('ROLE_EDITOR')) {
@@ -39,26 +75,7 @@ class RecipeController extends AbstractController
             /** @var UploadedFile $imageFile */
             $imageFile = $form->get('imageUrl')->getData();
 
-            if ($imageFile) {
-                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
-                // this is needed to safely include the file name as part of the URL
-                $safeFilename = $slugger->slug($originalFilename);
-                $newFilename = $safeFilename.'-'.uniqid().'.'.$imageFile->guessExtension();
-
-                // Move the file to the directory where brochures are stored
-                try {
-                    $imageFile->move(
-                        $this->getParameter('upload_directory'),
-                        $newFilename
-                    );
-                } catch (FileException $e) {
-                    // ... handle exception if something happens during file upload
-                }
-
-                // updates the 'imageFilename' property to store the PDF file name
-                // instead of its contents
-                $recipe->setImageUrl($newFilename);
-            }
+            if ($imageFile) $recipe->setImageUrl($this->handleThumbnailInput($recipe, $imageFile));
 
             foreach ($recipe->getRecipeIngredients() as $recipeIngredient) {
                 $recipeIngredient->setRecipe($recipe);
@@ -119,7 +136,7 @@ class RecipeController extends AbstractController
 
     #[IsGranted('ROLE_EDITOR')]
     #[Route('/{id}/edit', name: 'app_recipe_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Recipe $recipe, RecipeRepository $recipeRepository): Response
+    public function edit(Request $request, Recipe $recipe, RecipeRepository $recipeRepository, SluggerInterface $slugger): Response
     {
         if ($this->getUser() !== $recipe->getAuthor()) return $this->redirectToRoute('app_home', status: 401);
 
@@ -127,6 +144,12 @@ class RecipeController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+
+            /** @var UploadedFile $imageFile */
+            $imageFile = $form->get('imageUrl')->getData();
+
+            if ($imageFile) $recipe->setImageUrl($this->handleThumbnailInput($recipe, $imageFile));
+
             $recipeRepository->add($recipe, true);
 
             return $this->redirectToRoute('app_recipe_show', [ 'id' => $recipe->getId() ], Response::HTTP_SEE_OTHER);
