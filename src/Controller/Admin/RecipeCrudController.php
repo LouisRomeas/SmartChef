c2a9aa961c63c6301fa5713b20204f5b82a7267b
Louis Roméas <louis.romeas@gmail.com>
2022-09-19 15:02:24 +0200

    EasyAdmin titles translations missing

--- src/Controller/Admin/RecipeCrudController.php
+++ src/Controller/Admin/RecipeCrudController.php
@@ -3,20 +3,29 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Recipe;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class RecipeCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
     {
         return Recipe::class;
+    }
+    
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('entity.recipe._singular')
+            ->setEntityLabelInPlural('entity.recipe._plural')
+        ;
     }
 
     public function configureFields(string $pageName): iterable
--- src/Controller/Admin/ReportCrudController.php
+++ src/Controller/Admin/ReportCrudController.php
@@ -3,16 +3,25 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Report;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class ReportCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
     {
         return Report::class;
+    }
+    
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('entity.report._singular')
+            ->setEntityLabelInPlural('entity.report._plural')
+        ;
     }
 
     public function configureFields(string $pageName): iterable
